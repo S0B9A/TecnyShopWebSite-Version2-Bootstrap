c9c02924f2eb5d31138eb19b4c1773306f35b2b0
Sebastian Arias <sebasxml9@gmail.com>
2024-07-29 17:32:56 -0600

    Se agrega el Json funcional al grafico de inicio

--- JavaScript/Inicio.js
+++ JavaScript/Inicio.js
@@ -1,12 +1,13 @@
-const labels = ['Atención al Cliente', 'Calidad del Producto', 'Satisfacción del Cliente'];
-const colors = ['rgb(255,165,0)', 'rgb(255,140,0)', 'rgb(255,99,71)'];
+
+const labels = valores.map(item => item.Nombre);
+const colors = valores.map(item => item.color);
 
 const graph = document.querySelector("#grafica");
 
 const data = {
     labels: labels,
     datasets: [{
-        data: [33, 33, 34], // Los porcentajes suman 100%
+        data: valores.map(() => 100 / valores.length), // Distribuir porcentajes equitativamente
         backgroundColor: colors
     }]
 };
---
+++ Json/valores.js
@@ -0,0 +1,18 @@
+const valores=
+[
+    {
+        "ID": 1,
+        "Nombre": "Atención al Cliente",
+        "color": "rgb(255,165,0)"
+    },
+    {
+        "ID": 2,
+        "Nombre": "Calidad del Producto",
+        "color": "rgb(255,140,0)"
+    },
+    {
+        "ID": 3,
+        "Nombre": "Satisfacción del Cliente",
+        "color": "rgb(255,99,71)"
+    }
+]
