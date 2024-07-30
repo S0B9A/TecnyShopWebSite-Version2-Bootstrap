632cf34e5c5d0abe41d2f389c17cfe3fc8951bed
Sebastian Arias <sebasxml9@gmail.com>
2024-07-29 18:37:56 -0600

    Se agrega la pagina de preguntas frecuentes

--- Json/products.js
+++ Json/products.js
@@ -10,7 +10,7 @@ const products=
         "Nombre": "Monitor Curvo",
         "Categoria":"Oficina",
         "Precio":150000,
-        "Galeria":["/Images/Productos/Oficina/Monitor Curvo 34 pulgadas.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Oficina/Monitor Curvo 34 pulgadas.png","./Images/Productos/image-not-found.jpg"],
         "Stock":true,
         "Descripcion":"Monitor de 34 pulgadas",
         "Garantia":"1 anno"
@@ -20,7 +20,7 @@ const products=
         "Nombre": "Monitor de Escritorio 1C",
         "Categoria":"Oficina",
         "Precio":160000,
-        "Galeria":["/Images/Productos/Oficina/Monitor de escritorio 1C Xiaomi 23.8 pulgadas.jpg","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Oficina/Monitor de escritorio 1C Xiaomi 23.8 pulgadas.jpg","./Images/Productos/image-not-found.jpg"],
         "Stock":false,
         "Descripcion":"Monitor de 23.8 pulgadas",
         "Garantia":"1 anno"
@@ -30,7 +30,7 @@ const products=
         "Nombre": "Monitor Gaming 2K",
         "Categoria":"Oficina",
         "Precio":170000,
-        "Galeria":["/Images/Productos/Oficina/Monitor Gaming 2K 27 pulgadas.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Oficina/Monitor Gaming 2K 27 pulgadas.png","./Images/Productos/image-not-found.jpg"],
         "Stock":true,
         "Descripcion":"Monitor de 27 pulgadas",
         "Garantia":"1 anno"
@@ -40,7 +40,7 @@ const products=
         "Nombre": "Freidora de Aire",
         "Categoria":"Electrodomésticos",
         "Precio":70000,
-        "Galeria":["/Images/Productos/Electrodomésticos/FreidoraAire-6L-Photoroom.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Electrodomésticos/FreidoraAire-6L-Photoroom.png","./Images/Productos/image-not-found.jpg"],
         "Stock":false,
         "Descripcion":"6L",
         "Garantia":"1 anno"
@@ -50,7 +50,7 @@ const products=
         "Nombre": "Dispensador de Agua",
         "Categoria":"Electrodomésticos",
         "Precio":40000,
-        "Galeria":["/Images/Productos/Electrodomésticos/Dispensador de agua inteligente Xiaomi-Photoroom.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Electrodomésticos/Dispensador de agua inteligente Xiaomi-Photoroom.png","./Images/Productos/image-not-found.jpg"],
         "Stock":true,
         "Descripcion":"Inteligente",
         "Garantia":"1 anno"
@@ -60,7 +60,7 @@ const products=
         "Nombre": "Hervidor Mijia",
         "Categoria":"Electrodomésticos",
         "Precio":35000,
-        "Galeria":["/Images/Productos/Electrodomésticos/Hervidor Mijia PRO-Scene-Photoroom.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Electrodomésticos/Hervidor Mijia PRO-Scene-Photoroom.png","./Images/Productos/image-not-found.jpg"],
         "Stock":false,
         "Descripcion":"Mi Smart Pro",
         "Garantia":"1 anno"
@@ -70,7 +70,7 @@ const products=
         "Nombre": "Aspiradora de ácaros  y polvo",
         "Categoria":"Limpieza",
         "Precio":88000,
-        "Galeria":["/Images/Productos/Limpieza/Aspiradora de acaros y polvo-Photoroom.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Limpieza/Aspiradora de acaros y polvo-Photoroom.png","./Images/Productos/image-not-found.jpg"],
         "Stock":true,
         "Descripcion":"Nueva Edición",
         "Garantia":"1 anno"
@@ -80,7 +80,7 @@ const products=
         "Nombre": "Cepillo de dientes eléctrico",
         "Categoria":"Limpieza",
         "Precio":20000,
-        "Galeria":["/Images/Productos/Limpieza/Cepillo de dientes electrico T500-Photoroom.png","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Limpieza/Cepillo de dientes electrico T500-Photoroom.png","./Images/Productos/image-not-found.jpg"],
         "Stock":false,
         "Descripcion":"Edición T500",
         "Garantia":"1 anno"
@@ -90,7 +90,7 @@ const products=
         "Nombre": "Robot barredor y trapeador",
         "Categoria":"Limpieza",
         "Precio":115000,
-        "Galeria":["/Images/Productos/Limpieza/Robot barredor y trapeador T12.jpg","/Images/Productos/image-not-found.jpg"],
+        "Galeria":["/Images/Productos/Limpieza/Robot barredor y trapeador T12.jpg","./Images/Productos/image-not-found.jpg"],
         "Stock":true,
         "Descripcion":"Edición T12",
         "Garantia":"1 anno"
