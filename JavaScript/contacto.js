090bdc79bfec4f8919d2c7f26066a3e4ecebe73c
Sebastian Arias <sebasxml9@gmail.com>
2024-08-13 09:21:45 -0600

    Se agrega validaciones a la pagina de contacto

---
+++ JavaScript/contacto.js
@@ -0,0 +1,77 @@
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+
+    let isValid = true;
+
+    // Validación del Nombre Completo
+    const nombreCompleto = document.getElementById('nombreCompleto').value;
+    if (nombreCompleto.trim() === '') {
+        showError('El nombre completo es requerido.');
+        isValid = false;
+    }
+
+    // Validación del Email
+    const email = document.getElementById('email').value;
+    if (!email.includes('@') && !email.includes('.')) {
+        showError('El email no es válido.');
+        isValid = false;
+    }
+
+    // Validación del Teléfono
+    const telefono = document.getElementById('telefono').value;
+    const telefonoPattern = /^\+506\d{8}$/;
+    if (!telefonoPattern.test(telefono)) {
+        showError('El teléfono debe tener el formato +506 seguido de 8 dígitos. ejemplo "+506########"');
+        isValid = false;
+    }
+
+    // Validación de la Fecha de Nacimiento
+    const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    if (!fechaNacimiento) {
+        showError('La fecha de nacimiento es requerida.');
+        isValid = false;
+    }
+
+    // Validación de la Provincia
+    const provincia = document.getElementById('provincia').value;
+    if (provincia === '') {
+        showError('La provincia es requerida.');
+        isValid = false;
+    }
+
+    // Validación del Género
+    const generoMasculino = document.getElementById('masculino').checked;
+    const generoFemenino = document.getElementById('femenino').checked;
+    if (!generoMasculino && !generoFemenino) {
+        showError('El género es requerido.');
+        isValid = false;
+    }
+
+    // Validación del Mensaje
+    const mensaje = document.getElementById('mensaje').value;
+    if (mensaje.trim() === '') {
+        showError('El mensaje es requerido.');
+        isValid = false;
+    }
+
+    if (isValid) {
+       showError("Formulario enviado con éxito.");
+    }
+});
+
+function showError(message) {
+    // Asegúrate de que la alerta esté inicialmente oculta
+    const alert = document.querySelector('.alert');
+    alert.classList.add('hide');
+    alert.textContent = message;
+
+    // Mostrar la alerta
+    setTimeout(function () {
+        alert.classList.remove('hide'); // Mostrar la alerta
+    }, 100); // Esperar un poco para asegurarse de que se muestra
+
+    // Ocultar la alerta después de 2 segundos
+    setTimeout(function () {
+        alert.classList.add('hide');
+    }, 3000);
+}
